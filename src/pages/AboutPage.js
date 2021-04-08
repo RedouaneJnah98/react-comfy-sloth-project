79cc82ab9136587675d119a728af9706f528edc6
Redouane Jnah <redouanejnah2000@gmail.com>
2021-04-08 11:47:58 +0100

    setup home page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,31 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+            possimus accusantium quod modi eligendi vel odit harum, provident ut
+            qui, aliquid temporibus rerum accusamus maiores dolorem porro, atque
+            ex explicabo. Dolorem nam a molestias voluptas sequi expedita
+            sapiente consequatur eum in porro dicta, deserunt qui laborum sunt
+            obcaecati accusantium tempore quos alias neque animi soluta eius
+            pariatur. Amet mollitia hic delectus placeat inventore ipsam dolorum
+            perspiciatis vero accusantium similique et vel unde odio
+            necessitatibus itaque, error voluptates molestiae iure tempora.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
 }
 
 const Wrapper = styled.section`
