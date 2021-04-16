85df5cb5206812e48132f76f8170854d65b06251
Redouane Jnah <redouanejnah2000@gmail.com>
2021-04-16 11:58:56 +0000

    add checkout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import logo from '../assets/logo.svg';
-import { FaBars } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { links } from '../utils/constants';
-import CartButtons from './CartButtons';
-import { useProductsContext } from '../context/products_context';
-import { useUserContext } from '../context/user_context';
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo.svg'
+import { FaBars } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { links } from '../utils/constants'
+import CartButtons from './CartButtons'
+import { useProductsContext } from '../context/products_context'
+import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  const { openSidebar } = useProductsContext();
+  const { openSidebar } = useProductsContext()
+  const { myUser } = useUserContext()
 
   return (
     <NavContainer>
@@ -24,19 +25,24 @@ const Nav = () => {
         </div>
         <ul className="nav-links">
           {links.map((link) => {
-            const { id, text, url } = link;
+            const { id, text, url } = link
             return (
               <li key={id}>
                 <Link to={url}>{text}</Link>
               </li>
-            );
+            )
           })}
+          {myUser && (
+            <li>
+              <Link to="/checkout">checkout</Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </div>
     </NavContainer>
-  );
-};
+  )
+}
 
 const NavContainer = styled.nav`
   height: 5rem;
@@ -103,6 +109,6 @@ const NavContainer = styled.nav`
       display: grid;
     }
   }
-`;
+`
 
-export default Nav;
+export default Nav
