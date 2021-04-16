d961f27106d37764512ed94914047a9ba94fb46b
Redouane Jnah <redouanejnah2000@gmail.com>
2021-04-16 12:58:17 +0000

    fix bugs & warnings

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,24 +3,14 @@ import { useAuth0 } from '@auth0/auth0-react'
 
 const UserContext = React.createContext()
 export const UserProvider = ({ children }) => {
-  const {
-    isAuthenticated,
-    loginWithRedirect,
-    logout,
-    user,
-    isLoading,
-  } = useAuth0()
+  const { loginWithRedirect, logout, user } = useAuth0()
 
   const [myUser, setMyUser] = useState(null)
 
   useEffect(() => {
-    if (isAuthenticated) {
-      setMyUser(user)
-    } else {
-      setMyUser(false)
-    }
+    setMyUser(user)
     // eslint-disable-next-line
-  }, [isAuthenticated])
+  }, [user])
 
   return (
     <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
